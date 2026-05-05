af5e0179464b85321365b0a509ca60112318fa98
surajpoudel-Omaha <102336510+surajpoudel-Omaha@users.noreply.github.com>
2026-05-05 17:44:26 -0500

    Update main.py
    added some changes

--- src/main.py
+++ src/main.py
@@ -64,7 +64,7 @@ if view == "Unemployment DashBoard":
     unEmployment = unEmployment.sort_values("date")
 
 
-    latest = unEmployment.sort_values(["year", "month"]).dropna().iloc[-1]
+    latest = unEmployment.dropna().iloc[-1]
 
     ### Unemployment DashBoard
     nonFarmWorkers = nonFarmersWorkers.sort_values(["year", "month"]).dropna().iloc[-1]
